4a533365588825ddeb0a0087bdf7d29a214a83df
sar0868 <safarov-ar@yandex.ru>
2024-08-17 16:18:26 +0300

    Homework_09: added methods

--- src/main/java/ru/otus/java/basic/safarov/homework09/Main.java
+++ src/main/java/ru/otus/java/basic/safarov/homework09/Main.java
@@ -1,12 +1,10 @@
 package ru.otus.java.basic.safarov.homework09;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-
     }
 
     public static List<Integer> listRange(int min, int max) {
@@ -28,8 +26,63 @@ public class Main {
     }
 
     public static void fillListValue(List<Integer> list, int value){
-        for (int items: list){
-            items = value;
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, value);
         }
     }
+
+    public static void increaseList(List<Integer> list, int value) {
+//        list.replaceAll(el -> el + value);
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, list.get(i) + value);
+        }
+    }
+
+    public static List<String> getNameEmployees(List<Employee> employees) {
+        List<String> names = new ArrayList<>();
+        for(Employee employee: employees){
+            names.add(employee.getName());
+        }
+        return names;
+    }
+
+    public static List<Employee> getEmployeesOld(
+            List<Employee> employees, int minAge){
+        List<Employee> result = new ArrayList<>();
+        for (Employee employee: employees){
+            if (employee.getAge() >= minAge){
+                result.add(employee);
+            }
+        }
+        return result;
+    }
+
+    public static boolean isAboveAvgAge(
+            List<Employee> employees, int minAvg){
+        int sumAge = 0;
+        for (Employee employee: employees){
+            sumAge += employee.getAge();
+        }
+        return (sumAge / employees.size()) > minAvg;
+    }
+    
+    public static Employee getYoungEmployee(
+            List<Employee> employees){
+        Employee result = null;
+        int minAge = -1;
+        for (Employee employee: employees){
+            if (minAge == -1){
+                result = employee;
+                minAge = employee.getAge();
+                continue;
+            }
+            if (employee.getAge() < minAge){
+                result = employee;
+                minAge = employee.getAge();
+            }
+                
+        }
+        return result;
+    }
+
 }
--- src/test/java/ru/otus/java/basic/safarov/homework09/MainTest.java
+++ src/test/java/ru/otus/java/basic/safarov/homework09/MainTest.java
@@ -2,7 +2,10 @@ package ru.otus.java.basic.safarov.homework09;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -12,16 +15,85 @@ class MainTest {
     void listRange() {
         int min = 2;
         int max = 5;
-        List<Integer> list;
-
-//        assertEqulas(1,Main.listRange(min, max));
+        List<Integer> list = List.of(2, 3, 4, 5);
+        assertEquals(list, Main.listRange(min, max));
     }
 
     @Test
     void sumItemGtFive() {
+        List<Integer> list = List.of(4, 5, 6);
+        assertEquals(6, Main.sumItemGtFive(list));
     }
 
     @Test
     void fillListValue() {
+        int value = 2;
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3));
+        Main.fillListValue(list, value);
+        assertEquals(new ArrayList<>(Arrays.asList(2, 2, 2)), list);
+    }
+
+    @Test
+    void increaseList(){
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3));
+        int value = 1;
+        Main.increaseList(list, value);
+        assertEquals(new ArrayList<>(Arrays.asList(2, 3, 4)), list);
+    }
+
+    @Test
+    void getNameEmployees(){
+        List<Employee> employees = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            employees.add(new Employee("name" + (i+1), (int)((Math.random() * (65-18)) + 18 )));
+        }
+        List<String> list = List.of("name1", "name2", "name3");
+        assertEquals(list, Main.getNameEmployees(employees));
+    }
+
+    @Test
+    void getEmployeesOld(){
+        List<Employee> employees = new ArrayList<>();
+        int[] ages = {18, 20, 21};
+        int minAge = 20;
+        for (int i = 0; i < 3; i++) {
+            employees.add(new Employee("name" + (i+1), ages[i]));
+        }
+        List<Employee> result = Main.getEmployeesOld(employees, minAge);
+        assertEquals(2, result.size());
+        assertEquals(21, result.get(1).getAge());
+        assertEquals(20, result.get(0).getAge());
+    }
+
+    @Test
+    void isAboveAvgAgeTrue(){
+        List<Employee> employees = new ArrayList<>();
+        int[] ages = {19, 20, 21};
+        int minAvgAge = 19;
+        for (int i = 0; i < 3; i++) {
+            employees.add(new Employee("name" + (i+1), ages[i]));
+        }
+        assertTrue(Main.isAboveAvgAge(employees, minAvgAge));
+    }
+
+    @Test
+    void isAboveAvgAgeFalse(){
+        List<Employee> employees = new ArrayList<>();
+        int[] ages = {19, 20, 21};
+        int minAvgAge = 20;
+        for (int i = 0; i < 3; i++) {
+            employees.add(new Employee("name" + (i+1), ages[i]));
+        }
+        assertFalse(Main.isAboveAvgAge(employees, minAvgAge));
+    }
+
+    @Test
+    void getYoungEmployee(){
+        List<Employee> employees = new ArrayList<>();
+        int[] ages = {19, 20, 21};
+        for (int i = 0; i < 3; i++) {
+            employees.add(new Employee("name" + (i+1), ages[i]));
+        }
+        assertEquals(employees.get(0), Main.getYoungEmployee(employees));
     }
 }
