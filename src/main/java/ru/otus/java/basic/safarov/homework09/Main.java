6a9ca76a86295f302d059eff297d734aa178e808
sar0868 <safarov-ar@yandex.ru>
2024-08-17 12:23:28 +0300

    Homework_09: added method fillListValue

--- src/main/java/ru/otus/java/basic/safarov/homework09/Main.java
+++ src/main/java/ru/otus/java/basic/safarov/homework09/Main.java
@@ -1,6 +1,7 @@
 package ru.otus.java.basic.safarov.homework09;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -8,15 +9,15 @@ public class Main {
 
     }
 
-    public static ArrayList<Integer> listRange(int min, int max) {
-        ArrayList<Integer> result = new ArrayList<>();
+    public static List<Integer> listRange(int min, int max) {
+        List<Integer> result = new ArrayList<>();
         for (int i = min; i <= max; i++) {
             result.add(i);
         }
         return result;
     }
 
-    public static int sumItemGtFive(ArrayList<Integer> list) {
+    public static int sumItemGtFive(List<Integer> list) {
         int sum = 0;
         for (int item : list) {
             if (item > 5) {
@@ -24,5 +25,11 @@ public class Main {
             }
         }
         return sum;
+    }
+
+    public static void fillListValue(List<Integer> list, int value){
+        for (int items: list){
+            items = value;
+        }
     }
 }
---
+++ src/test/java/ru/otus/java/basic/safarov/homework09/MainTest.java
@@ -0,0 +1,27 @@
+package ru.otus.java.basic.safarov.homework09;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+class MainTest {
+
+    @Test
+    void listRange() {
+        int min = 2;
+        int max = 5;
+        List<Integer> list;
+
+//        assertEqulas(1,Main.listRange(min, max));
+    }
+
+    @Test
+    void sumItemGtFive() {
+    }
+
+    @Test
+    void fillListValue() {
+    }
+}
