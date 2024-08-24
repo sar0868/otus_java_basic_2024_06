c8f4c31e16b8480e68926965060a2ed34e9d3203
sar0868 <safarov-ar@yandex.ru>
2024-08-24 11:36:12 +0300

    hw10: done

--- src/main/java/ru/otus/java/basic/safarov/homework10/PhoneBook.java
+++ src/main/java/ru/otus/java/basic/safarov/homework10/PhoneBook.java
@@ -3,27 +3,27 @@ package ru.otus.java.basic.safarov.homework10;
 import java.util.*;
 
 public class PhoneBook {
-    private final Map<Person, Set<Integer>> phonebook;
+    private final Map<Person, Set<String>> phonebook;
 
     public PhoneBook() {
         this.phonebook = new HashMap<>();
     }
 
-    public void add(Person name, int number){
-        if (name == null || number <= 0){
+    public void add(Person name, String number){
+        if (name == null || Long.parseLong(number) <= 0){
             throw new PhoneBookDateException("Не корректные данные для ввода в телефонную книгу.");
         }
         if (phonebook.containsKey(name)){
             phonebook.get(name).add(number);
         } else {
-            Set<Integer> numbers = new HashSet<>();
+            Set<String> numbers = new HashSet<>();
             numbers.add(number);
             phonebook.put(name, numbers);
         }
     }
 
     public String find(String surname){
-        Set<Integer> results = new HashSet<>();
+        Set<String> results = new HashSet<>();
         for (Person person: phonebook.keySet()){
             if (person.getSurname().equals(surname)){
                 results.addAll(phonebook.get(person));
@@ -33,15 +33,15 @@ public class PhoneBook {
             return "В телефонной книге нет человека с фамилией " + surname;
         }
         StringBuilder result = new StringBuilder();
-        for (int el: results){
+        for (String el: results){
             result.append(el).append(" ");
         }
         return result.toString();
     }
 
-    public boolean containsPhoneNumber(int number){
-        Collection<Set<Integer>> values = phonebook.values();
-        for (Set<Integer> items: values){
+    public boolean containsPhoneNumber(String number){
+        Collection<Set<String>> values = phonebook.values();
+        for (Set<String> items: values){
             if (items.contains(number)){
                 return true;
             }
@@ -49,7 +49,7 @@ public class PhoneBook {
         return false;
     }
 
-    public Map<Person, Set<Integer>> getPhonebook() {
+    public Map<Person, Set<String>> getPhonebook() {
         return phonebook;
     }
 }
--- src/test/java/ru/otus/java/basic/safarov/homework10/PhoneBookTest.java
+++ src/test/java/ru/otus/java/basic/safarov/homework10/PhoneBookTest.java
@@ -17,38 +17,38 @@ class PhoneBookTest {
     void add() {
         assertEquals(0, phoneBook.getPhonebook().size());
 
-        phoneBook.add(new Person("name", "surname", "patronymic"), 123456);
+        phoneBook.add(new Person("name", "surname", "patronymic"), "123456");
         assertEquals(1, phoneBook.getPhonebook().size());
     }
 
     @Test
     void find() {
-        phoneBook.add(new Person("name", "surname", "patronymic"), 123456);
-        phoneBook.add(new Person("name2", "surname", "patronymic"), 100000);
-        phoneBook.add(new Person("name", "surname2", "patronymic"), 444444);
+        phoneBook.add(new Person("name", "surname", "patronymic"), "123456");
+        phoneBook.add(new Person("name2", "surname", "patronymic"), "100000");
+        phoneBook.add(new Person("name", "surname2", "patronymic"), "444444");
         assertEquals("123456 100000 ", phoneBook.find("surname"));
     }
 
     @Test
     void findError() {
-        phoneBook.add(new Person("name", "surname", "patronymic"), 123456);
+        phoneBook.add(new Person("name", "surname", "patronymic"), "123456");
         assertEquals("В телефонной книге нет человека с фамилией user2",phoneBook.find("user2"));
     }
 
 
     @Test
     void containsPhoneNumber(){
-        phoneBook.add(new Person("name", "surname", "patronymic"), 123456);
-        phoneBook.add(new Person("name", "surname", "patronymic"), 100000);
-        phoneBook.add(new Person("name", "surname2", "patronymic"), 444444);
-        assertTrue(phoneBook.containsPhoneNumber(100000));
+        phoneBook.add(new Person("name", "surname", "patronymic"), "123456");
+        phoneBook.add(new Person("name", "surname", "patronymic"), "100000");
+        phoneBook.add(new Person("name", "surname2", "patronymic"), "444444");
+        assertTrue(phoneBook.containsPhoneNumber("100000"));
     }
 
     @Test
     void containsPhoneNumberFalse(){
-        phoneBook.add(new Person("name", "surname", "patronymic"), 123456);
-        phoneBook.add(new Person("name", "surname", "patronymic"), 100000);
-        phoneBook.add(new Person("name", "surname2", "patronymic"), 444444);
-        assertFalse(phoneBook.containsPhoneNumber(999999));
+        phoneBook.add(new Person("name", "surname", "patronymic"), "123456");
+        phoneBook.add(new Person("name", "surname", "patronymic"), "100000");
+        phoneBook.add(new Person("name", "surname2", "patronymic"), "444444");
+        assertFalse(phoneBook.containsPhoneNumber("999999"));
     }
 }
