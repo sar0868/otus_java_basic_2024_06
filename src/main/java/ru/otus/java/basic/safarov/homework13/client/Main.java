31f95e59976ff1c1964de057aa3b0f0889cf5765
sar0868 <safarov-ar@yandex.ru>
2024-09-04 09:37:07 +0300

    hw13: added information about calculator operations

--- src/main/java/ru/otus/java/basic/safarov/homework13/client/Main.java
+++ src/main/java/ru/otus/java/basic/safarov/homework13/client/Main.java
@@ -4,12 +4,14 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+    static Scanner scanner = new Scanner(System.in);
 
+
+    public static void main(String[] args) {
         try (Socket socket = new Socket("localhost", 8888);
-             PingClient client = new PingClient(socket)) {
+            PingClient client = new PingClient(socket)) {
+            System.out.println(client.read());
+            String input = scanner.nextLine();
             client.send(input);
             System.out.println(client.read());
         } catch (Exception e) {
--- src/main/java/ru/otus/java/basic/safarov/homework13/server/Main.java
+++ src/main/java/ru/otus/java/basic/safarov/homework13/server/Main.java
@@ -5,62 +5,15 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-    private static final String msgError = "Неверный запрос.";
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8888);
-        while (true){
+        while (true) {
             Socket clientSocket = serverSocket.accept();
-//            System.out.println("Client connect");
-            readRequest(clientSocket);
-        }
-    }
-
-    private static void readRequest(Socket socket) throws IOException {
-        DataInputStream input = new DataInputStream(
-                new BufferedInputStream(socket.getInputStream()));
-        DataOutputStream out = new DataOutputStream(
-                new BufferedOutputStream(socket.getOutputStream()));
-        String data = input.readUTF();
-        data = data.replaceAll("\\s+", " ");
-        String[] dataArray = data.trim().split(" ");
-        if (dataArray.length != 3){
-            out.writeUTF(msgError);
-            out.flush();
-        } else {
-            try {
-                String result = dataArray[0] + " " + dataArray[2] + " "
-                        + dataArray[1] + " = " + calculate(dataArray);
-                out.writeUTF(result);
-                out.flush();
-            } catch (ArithmeticException | IllegalArgumentException e){
-                out.writeUTF(e.getMessage());
-            } finally {
-                out.flush();
-            }
-        }
-    }
-
-    private static int calculate(String[] dataArray){
-        try{
-            int a = Integer.parseInt(dataArray[0]);
-            int b = Integer.parseInt(dataArray[1]);
-            String operation = dataArray[2];
-            switch (operation){
-                case "+": return a + b;
-                case "-": return a - b;
-                case "*": return a * b;
-                case "/": {
-                    if (b == 0){
-                        throw new ArithmeticException("divide by zero");
-                    }
-                    return a / b;
-                }
-                default:
-                    throw new IllegalArgumentException("Incorrect input data");
-            }
-        } catch (NumberFormatException e){
-            throw new NumberFormatException("Input data don't number");
+            System.out.println("Client connect");
+            ServerHandler serverHandler = new ServerHandler(clientSocket);
+            serverHandler.info();
+            serverHandler.readRequest();
         }
     }
 }
