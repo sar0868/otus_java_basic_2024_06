066328d5a6e0869eac92a0fd00ac87346ff0b304
sar0868 <safarov-ar@yandex.ru>
2024-09-13 13:20:47 +0300

    hw15: create branch

--- src/main/java/ru/otus/java/basic/safarov/homework15/client/Main.java
+++ src/main/java/ru/otus/java/basic/safarov/homework15/client/Main.java
@@ -1,7 +1,21 @@
 package ru.otus.java.basic.safarov.homework15.client;
 
-public class Main {
-    public static void main(String[] args) {
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Main {
+    static  Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (true){
+            try (Socket socket = new Socket("127.0.0.1", 8888);
+                 PingClient client = new PingClient(socket)){
+                System.out.println(client.read());
+                String msg = scanner.nextLine();
+                client.seng(msg);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
--- src/main/java/ru/otus/java/basic/safarov/homework15/client/PingClient.java
+++ src/main/java/ru/otus/java/basic/safarov/homework15/client/PingClient.java
@@ -1,4 +1,32 @@
 package ru.otus.java.basic.safarov.homework15.client;
 
-public class PingClient {
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class PingClient implements AutoCloseable {
+    private final DataInputStream inputStream;
+    private final DataOutputStream outputStream;
+
+    public PingClient(Socket socket) throws IOException {
+        this.inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        this.outputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+    }
+
+    public void seng(String text) throws IOException {
+        outputStream.write(text.getBytes(StandardCharsets.UTF_8));
+        outputStream.flush();
+    }
+
+    public String read() throws IOException {
+//        String answer = new String(inputStream.readUTF());
+//        System.out.println(answer);
+        return inputStream.readUTF();
+    }
+
+    @Override
+    public void close() throws Exception {
+        inputStream.close();
+        outputStream.close();
+    }
 }
