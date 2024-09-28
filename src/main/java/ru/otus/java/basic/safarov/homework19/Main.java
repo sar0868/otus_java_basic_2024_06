5cae96b931d934d106d58f52a83e1f3e0160a08b
sar0868 <safarov-ar@yandex.ru>
2024-09-28 14:13:11 +0300

    hw19: create branch

---
+++ src/main/java/ru/otus/java/basic/safarov/homework19/Main.java
@@ -0,0 +1,8 @@
+package ru.otus.java.basic.safarov.homework19;
+
+public class Main {
+    public static void main(String[] args) {
+        Fruit fruit = new Fruit(23);
+        System.out.println(fruit.getWeight());
+    }
+}
